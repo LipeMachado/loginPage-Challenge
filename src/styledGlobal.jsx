0ee88e0792baa3171aa2fb8d4d081f83819dc2b1
LipeMachado <silvalipe765@gmail.com>
2022-07-11 01:49:32 -0300

    Style Fix

--- src/styledGlobal.jsx
+++ src/styledGlobal.jsx
@@ -6,12 +6,20 @@ export const GlobalStyle = createGlobalStyle`
         --white: #FFFFFF;
         --white-200: #F7FAFC;
         --gray: #1A202C;
+        --gray-light: #F7FAFC;
+        --gray-border: #E8E8E8;
         --green-light: #04C35C;
         --purple-light: #2B6CB0;  
+        --font-merri: 'Merriweather', serif;
+        --font-lato: 'Lato', sans-serif;
     }
 
     * {
         margin: 0;
         padding: 0;
+    }
+
+    body {
+        font-family: 'Roboto', sans-serif;
     }
 `;
